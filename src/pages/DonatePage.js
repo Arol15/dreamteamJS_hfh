402c196b5b8d01db7154f98b4542494cd2f1a23a
Lora Rusinouskaya <lrusinouskaya@outlook.com>
2023-09-30 20:43:36 -0400

    add header

--- src/pages/DonatePage.js
+++ src/pages/DonatePage.js
@@ -41,96 +41,99 @@ export default function DonatePage() {
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <div>
-        <label htmlFor="name">Name:</label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          value={formData.name}
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="company_name">Company Name:</label>
-        <input
-          type="text"
-          id="company_name"
-          name="company_name"
-          value={formData.company_name}
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="address_1">Address Line 1:</label>
-        <input
-          type="text"
-          id="address_1"
-          name="address_1"
-          value={formData.address_1}
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="address_2">Address Line 2:</label>
-        <input
-          type="text"
-          id="address_2"
-          name="address_2"
-          value={formData.address_2}
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="city">City:</label>
-        <input
-          type="text"
-          id="city"
-          name="city"
-          value={formData.city}
-          required
-          onChange={handleInputChange}
-        />
-      </div>
-      <div>
-        <label htmlFor="zip">Zip code:</label>
-        <input
-          type="text"
-          id="zip"
-          name="zip"
-          value={formData.zip}
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="description">Description:</label>
-        <textarea
-          type="text"
-          id="description"
-          name="description"
-          value={formData.description}
-          onChange={handleInputChange}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="image">Image:</label>
-        <input
-          type="file"
-          id="image"
-          name="image"
-          accept="image/*"
-          required
-          onChange={handleImageUpload}
-        />
-      </div>
-      <button type="submit">Submit</button>
-    </form>
+    <>
+      <h1>Donate Today!</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="company_name">Company Name:</label>
+          <input
+            type="text"
+            id="company_name"
+            name="company_name"
+            value={formData.company_name}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="address_1">Address Line 1:</label>
+          <input
+            type="text"
+            id="address_1"
+            name="address_1"
+            value={formData.address_1}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="address_2">Address Line 2:</label>
+          <input
+            type="text"
+            id="address_2"
+            name="address_2"
+            value={formData.address_2}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="city">City:</label>
+          <input
+            type="text"
+            id="city"
+            name="city"
+            value={formData.city}
+            required
+            onChange={handleInputChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="zip">Zip code:</label>
+          <input
+            type="text"
+            id="zip"
+            name="zip"
+            value={formData.zip}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="description">Description:</label>
+          <textarea
+            type="text"
+            id="description"
+            name="description"
+            value={formData.description}
+            onChange={handleInputChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="image">Image:</label>
+          <input
+            type="file"
+            id="image"
+            name="image"
+            accept="image/*"
+            required
+            onChange={handleImageUpload}
+          />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </>
   );
 }
