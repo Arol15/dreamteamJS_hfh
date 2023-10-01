611379af1910459c9042655ab832eee141027337
Christian <christiandarlington0@gmail.com>
2023-10-01 03:25:59 -0400

    latest design

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,68 +1,80 @@
-import { useState, useEffect } from "react"
-
+import { useState, useEffect } from 'react'
 
 const Admins = () => {
+  const [postings, setPosting] = useState('')
 
-  const [data, setData] = useState('')
+  
+useEffect(() => {
 
-  const URL = `https://us-central1-data-253523.cloudfunctions.net/posting`
+    console.log("hey there")
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(URL);
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const jsonData = await response.json();
-        setData(jsonData);
-      } catch (error) {
-        // Handle errors here
-        console.error('Error fetching data:', error);
+    fetch("https://corsproxy.io/?https://us-central1-data-253523.cloudfunctions.net/posting",{
+      method: 'POST',
+      // mode: 'cors',
+      headers: {
+        Host: "us-central1-data-253523.cloudfunctions.net",
+        "Content-Type": "application/json",
       }
-    };
+    , body: JSON.stringify({"certified": true})
+      
+    })
+      .then(res => res.json())
+      .then(data => {
+        setPosting(data)
+        console.log("you got data", data)           
+      })
+      .catch(err => {
+        console.log("error...oh no");
+        console.error(err)
+      })
+    // if (!response.ok) {
+    //   throw new Error('Network response was not ok');
+    // }
+    // const jsonData = await response.json();
+    // setData(jsonData);
+    // } catch (error) {
+    //   // Handle errors here
+    //   console.error('Error fetching data:', error)
+    // }
+    // };
 
-    console.log(data)
-  
     // Call fetchData to initiate the data fetching
-    
-  }, []);
-  
+  }, [])
+
+
+
   return (
     <>
-    {/* <div className="flex flex-wrap justify-center">
-      {cardsData.map((card, index) => (
-        <div key={index} className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 flex mx-8 my-4 mt-24">     
-            <img
-              className="rounded-t-lg"
-              src="/docs/images/blog/image-1.jpg"
-              alt=""
-            />        
-          <div className="p-5">      
-              <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{card.title}</h5>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{card.description}</p>
-            <div className="flex">
-            <button
-              href="#"
-              className="inline-flex items-center px-4 py-3 mr-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Recieved
-            </button>
-            <button
-              href="#"
-              className="inline-flex items-center px-4 py-3 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            >
-              Certified
-            </button>
-            </div>
-          </div>
+      {/* {Array.isArray(postings) && postings.map(post => {
+  return <h1>{post.description}</h1>
+})} */}
+          
+  {postings && postings.map((post, index) => (
+    <div className='w-full mt-10' key={index}>
+    <div  className="max-w-md  border border-gray-200 rounded-lg shadow-lg bg-white dark:border-gray-700 mb-4">
+      <a href="/">
+        <img className="w-full rounded-t-lg" src={post.image_uri} alt="" />
+      </a>
+      <div className="p-5">
+        <a href="/">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-700 ">{post.city}</h5>
+        </a>
+        <p className="mb-3 font-bold dark:text-gray-600">{post.description}</p>
+        <div className="flex justify-between">
+          <button className="inline-flex items-center px-7 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+           Received
+          </button>
+          <button className="inline-flex items-center px-7 py-2 text-sm font-medium text-center text-white bg-green-500 rounded-lg hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">
+          Verify
+          </button>
         </div>
-      ))}
-    </div> */}
-  </>
-  
+    </div>
+      </div>
+    </div>
+  ))}
 
 
+    </>
   )
 }
 
--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -3,17 +3,17 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-     
-       <nav className="shadow-lg rounded-lg p-4 ">
-      <div className="">
-         <h1 className="font-bold text-4xl flex justify-end items-center mt-8 text-gray-800">Admin Portal</h1>
-        <div>
-          <a href="/" className="text-gray-700 hover:text-gray-900 text-lg font-semibold flex justify-center items-center">Shop</a>
-          <a href="/" className="text-gray-700 hover:text-gray-900 text-lg font-semibold">Donate</a>
-        </div>
+    <nav className="shadow-lg rounded-lg p-8 flex justify-between items-center">
+      <div className="flex items-center">
+        <h1 className="font-bold text-4xl text-gray-800 pl-8">Admin Portal</h1>
+      </div>
+      <div className="flex space-x-6">
+        <a href="/" className="text-gray-700 hover:text-gray-900 text-lg font-semibold pr-4">Donate</a>
+        <a href="/" className="text-gray-700 hover:text-gray-900 text-lg font-semibold">Shop</a>
       </div>
     </nav>
-    </div>
+  </div>
+  
   )
 }
 
